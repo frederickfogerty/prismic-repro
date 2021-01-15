e896ed9032c38d0c2442499b8e37a6c2fff25232
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-01-15 10:40:13 +0100

    Error repro

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,32 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: process.env.PRISMIC_REPO_NAME,
+
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+
+        schemas: {
+          page: require("./config/prismic/schemas/page.json"),
+          menu: require("./config/prismic/schemas/menu.json"),
+          homepage: require("./config/prismic/schemas/page.json"),
+        },
+
+        imageImgixParams: {
+          auto: "compress,format",
+          fit: "max",
+          q: 50,
+        },
+
+        imagePlaceholderImgixParams: {
+          w: 100,
+          blur: 15,
+          q: 50,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
